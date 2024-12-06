573e3ab33a9f927c284c2bc2584b90baa4d916ee
RiskEst <87439775+RiskEst@users.noreply.github.com>
2024-12-06 13:52:36 +0100

    ich bin verzweifelt

--- obj/Debug/net8.0/Demo.AssemblyInfo.cs
+++ obj/Debug/net8.0/Demo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Demo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+708c1bc197c63974437843356dd0f27753112c75")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5628be04b2d6a2032d13aabf5a5cc3cf7ea8635e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Demo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Demo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
